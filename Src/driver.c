695f905511e4f64a9ec0cc2e47b127a1eada881d
Terje Io <terjeio@users.noreply.github.com>
2025-04-11 02:21:24 +0200

    Fixed regression causing spindle encoder data to be reset shortly before starting spindle synced motion - resulting in error 41.

--- Src/driver.c
+++ Src/driver.c
@@ -2640,7 +2640,7 @@ bool driver_init (void)
 #else
     hal.info = "STM32F756";
 #endif
-    hal.driver_version = "250405";
+    hal.driver_version = "250411";
     hal.driver_url = GRBL_URL "/STM32F7xx";
 #ifdef BOARD_NAME
     hal.board = BOARD_NAME;
